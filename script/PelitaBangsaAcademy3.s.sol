e248a47bfc102fbf571e662d539575a1882f526f
Dimas Purwanto <dimas@jalintraksi.com>
2024-08-03 13:47:14 +0700

    initilize

---
+++ script/PelitaBangsaAcademy3.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {PelitaBangsaAcademy3} from "src/PelitaBangsaAcademy3.sol";
+
+contract PelitaBangsaAcademy3Script is Script {
+  function setUp() public {}
+
+  function run() public {
+    // TODO: Set addresses for the variables below, then uncomment the following section:
+    /*
+    vm.startBroadcast();
+    address initialOwner = <Set initialOwner address here>;
+    PelitaBangsaAcademy3 instance = new PelitaBangsaAcademy3(initialOwner);
+    console.log("Contract deployed to %s", address(instance));
+    vm.stopBroadcast();
+    */
+  }
+}
